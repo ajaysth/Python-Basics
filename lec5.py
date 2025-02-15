e225b81285f4db79b299a4a713cb4816014e6722
ajaysth <shrestha11ajay@gmail.com>
2025-02-15 17:01:43 +0545

    basics

--- lec5.py
+++ lec5.py
@@ -33,6 +33,27 @@ dicts={
 # print(list(dicts.keys())) #converts in to list
 # print(dicts.get("Interests"))
 
-newdict={"player":"messi","strong":True,}
-dicts.update(newdict)
-print(dicts)
+# newdict={"player":"messi","strong":True,}
+# dicts.update(newdict)
+# print(dicts)
+
+
+#SETS
+
+# data1={1,6,4,7}
+# print(data1)
+# print(type(data1))
+# data1.add(8)
+# data1.add("aj")
+# data1.add(8.88)
+# print(data1)
+# data1.remove(4)
+# print(data1)
+
+# data2 = set() #empty set syntax
+# print(type(data2))
+
+data1={1,6,4,7}
+data2={4,1,8,2}
+res=data1.union(data2)
+print(res)
