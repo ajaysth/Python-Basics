851e2852eaa00b0ba532bd1189db796879deeae7
ajaysth <shrestha11ajay@gmail.com>
2025-02-27 18:23:47 +0545

    Some parts

--- 2ns4.py
+++ 2ns4.py
@@ -3,8 +3,19 @@
 # f.write("I am ajayshrestha and ajay is very nice . ajay is a hero")
 # f.close()
 
-#now replace ajay with messi
+# #now replace ajay with messi
+# with open("demo.txt","r") as f:
+#     data=f.read()
+#     data2=data.replace("ajay","messi")  
+#     print(data2)
+
+
+# find the word learning exist or not
+word="hero"
 with open("demo.txt","r") as f:
     data=f.read()
-    data2=data.replace("ajay","messi")  
-    print(data2)
+    if(data.find(word)!=-1):
+        print("Found")
+    else:
+        print("Not found")    
+   
---
+++ oop1.py
@@ -0,0 +1,22 @@
+
+#class
+# class Teacher:
+#     name="Ajay"
+
+
+# #object 
+# t1=Teacher()
+# print(t1.name)    
+
+
+class Bike:
+    color="Red"
+    name="r15"
+
+b1=Bike()
+print(b1.color)
+print(b1.name)
+
+b2=Bike()
+print(b2.color)
+print(b2.name)
